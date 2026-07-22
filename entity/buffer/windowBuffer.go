570646c209036c86fc9933da717154230c50c88f
Soumen Mitra <Soumen.no.hako@gmail.com>
2026-07-23 00:48:37 +0530

    more buffer shape changes

--- entity/buffer/windowBuffer.go
+++ entity/buffer/windowBuffer.go
@@ -4,8 +4,17 @@ type ScreenBuffer struct {
     Height int
     Width int
     Font_size int
+    MenuBar []string   // Menu bar like [ FILE, EDIT, SETTINGS, HELP]
+    RowStart int       // main content will be start from this num. Example line 3 will be starting of main content
+    RowEnd int         //  "       "     "  end in this line. Example line 42 will be last shown line
+    RowCountBegin int  // This variable represents the line number of a large file content. Like line 23 has something writtem  At start
+    RowCountEnd int    //  "       "          "     "   "     "     "      "    "    "        "    "  75  "      "       "      At end. After thisline toolbar at bottom
+    InfoBar []string   // Represents absolute last line that shows mode name, row col position and other info
 }
 
 type WriterBuffer struct {
-    lines []int
+    lines []string
+}
+type FileBuffer struct {
+    lines []string
 }
